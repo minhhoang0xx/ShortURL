3ed32d0c5e844cbcb2aec0bf92409beb8a8bff5d
minhhoang0xx <118556260+minhhoang0xx@users.noreply.github.com>
2025-07-08 15:01:07 +0700

    🧹 Bỏ thư mục .vs khỏi repo

--- src/services/DownloadService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const download = async (filteredData) => {
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/Download/download`,filteredData,{responseType: "blob"});
-    return res; 
-}
--- src/services/TagService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-
-export const getAllTags = async () => {
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/Tag/tags`);
-    return res.data;
-  };
